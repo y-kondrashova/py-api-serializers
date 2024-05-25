c85b596a2192a968bbb707d6b11e4e536e35184b
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-25 09:37:52 +0300

    Add CinemaHallSerializer to serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,7 +1,6 @@
-# write serializers here
 from rest_framework import serializers
 
-from models import Actor
+from models import Actor, CinemaHall
 
 
 class ActorSerializer(serializers.Serializer):
@@ -18,6 +17,25 @@ class ActorSerializer(serializers.Serializer):
         )
         instance.last_name = validated_data.get(
             "last_name", instance.last_name
+        )
+        instance.save()
+        return instance
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_rows = validated_data.get(
+            "seats_in_rows", instance.seats_in_rows
         )
         instance.save()
         return instance
