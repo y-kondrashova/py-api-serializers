e8e11864d1c9727420d8898564c92f42493b13dc
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-25 10:03:36 +0300

    Add MovieSerializer to serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from models import Actor, CinemaHall, Genre
-
-from models import Actor, CinemaHall
+from models import Actor, CinemaHall, Genre, Movie
 
 
 class ActorSerializer(serializers.Serializer):
@@ -56,5 +54,24 @@ class GenreSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField(style={"type": "textarea"})
+    duration = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data(
+            "description", instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
