b674e4d62dba82c64b4d6c0cb5fbecf116dc492d
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-25 14:03:34 +0300

    Refactor serializers.py
    Fix flake8 errors:
     - ANN001 Missing type annotation for function argument 'instance
     - ANN001 Missing type annotation for function argument 'validated_data'
     - ANN201 Missing return type annotation for public function

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -9,10 +9,10 @@ class ActorSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=255)
     last_name = serializers.CharField(max_length=255)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Actor:
         return Actor.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Actor, validated_data: dict) -> Actor:
         instance.first_name = validated_data.get(
             "first_name", instance.first_name
         )
@@ -29,10 +29,10 @@ class CinemaHallSerializer(serializers.Serializer):
     rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> CinemaHall:
         return CinemaHall.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: CinemaHall, validated_data: dict) -> CinemaHall:
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_rows = validated_data.get(
@@ -49,10 +49,10 @@ class GenreSerializer(serializers.Serializer):
         validators=[UniqueValidator(queryset=Genre.objects.all())]
     )
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Genre:
         return Genre.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Genre, validated_data: dict) -> Genre:
         instance.name = validated_data.get("name", instance.name)
         instance.save()
         return instance
@@ -64,12 +64,12 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField(style={"type": "textarea"})
     duration = serializers.IntegerField()
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Movie:
         return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Movie, validated_data: dict) -> Movie:
         instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data(
+        instance.description = validated_data.get(
             "description", instance.description
         )
         instance.duration = validated_data.get("duration", instance.duration)
@@ -81,10 +81,12 @@ class MovieSessionSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     show_time = serializers.DateTimeField()
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> MovieSession:
         return MovieSession.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self,
+               instance: MovieSession,
+               validated_data: dict) -> MovieSession:
         instance.show_time = validated_data.get(
             "show_time", instance.show_time
         )
