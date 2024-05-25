aa176d4832b7f20130019e3367d9bcb6869224ce
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-25 09:26:59 +0300

    Add ActorSerializer to serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,23 @@
 # write serializers here
+from rest_framework import serializers
+
+from models import Actor
+
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name
+        )
+        instance.save()
+        return instance
