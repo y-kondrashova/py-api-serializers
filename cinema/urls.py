90b3f51bc017a44c5757f429d1f654160079ef98
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-27 08:54:57 +0300

    Add path to actors in urls.py cinema app

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,14 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import ActorViewSet
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+
+router.register("actors", ActorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
