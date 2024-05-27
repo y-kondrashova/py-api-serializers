d6458546925aa547fa808b2f564fdde7cc469ac6
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-27 10:53:26 +0300

    Add path to cinema_halls in urls.py cinema app

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import ActorViewSet
+from cinema.views import ActorViewSet, CinemaHallViewSet
 
 app_name = "cinema"
 
 router = routers.DefaultRouter()
 
 router.register("actors", ActorViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
