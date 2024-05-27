5c2654559dc5151e4e36e4cebb862d0a60ba8845
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-27 09:06:19 +0300

    Add CinemaHallViewSet to views.py cinema app

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from cinema.models import Actor
-from cinema.serializers import ActorSerializer
+from cinema.models import Actor, CinemaHall
+from cinema.serializers import ActorSerializer, CinemaHallSerializer
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
